707a6642aa43fadb0853ea3fd88441784ff76945
Hakan Ünal <hakanunal@devtagon.com>
2021-03-31 00:48:11 +0300

    edited

--- src/components/Design.js
+++ src/components/Design.js
@@ -1,7 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from "react";
-import { Card, Button, Row, Col } from 'react-bootstrap';
-import logo from '../logo.svg';
 
 
 export default class Design extends Component {
