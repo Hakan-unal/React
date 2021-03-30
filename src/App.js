69e2cdbcf92ffc66c347e7ce95b2b6a9632375fa
Hakan Ünal <hakanunal@devtagon.com>
2021-03-30 16:26:58 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Say hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  calculate = (operation) => {
+    let val;
+    if (operation === "increase") {
+      val = this.state.count
+      val++;
+    } else {
+      val = this.state.count;
+      val--;
+    }
+    this.setState({ count: val })
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1><em>Follow The White Rabbit...</em></h1>
+          <Row>
+            <div className="input-group">
+              <Button className="btn btn-info" onClick={() => this.calculate("increase")}>+</Button>
+              <Button className="btn btn-danger" onClick={() => this.calculate("decrease")}>-</Button>
+            </div>
+          </Row>
+          {this.state.count}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
