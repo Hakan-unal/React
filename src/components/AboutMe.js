34b0857a7397452789f616a4c50bdd38b098b135
Con Sınov <hakanunal@devtagon.com>
2022-04-15 18:44:42 +0300

    update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,7 +14,14 @@ export default class AboutMe extends Component {
             <div className="h3 text-dark text-center mt-5 mx-5">
                 Hey&#x1F44B;, It's Hakan. Currently working as Software Developer at
                 <a href="http://devtagon.com/" style={{ position: "relative" }} className="stretched-link font-weight-bold text-dark"> Devtagon</a>.Here you can learn more about me and my ideas.
+                <br />
+                <br />
+                <br />
+
+                <a href="https://hakanunal.vercel.app/" style={{ position: "relative" }} className="stretched-link font-weight-bold text-dark"> Vercel App</a>
+
             </div>
+
 
         );
     }
