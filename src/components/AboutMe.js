1e6009461de74807981e7f75601c2eb7d1e6807a
Con Sınov <hakanunal@devtagon.com>
2022-06-18 22:43:20 +0300

    updated

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,9 +12,12 @@ export default class AboutMe extends Component {
         return (
 
             <div className="h3 text-dark text-center mt-5 mx-5">
+
                 Hey&#x1F44B;, It's Hakan. Currently working as Software Developer at
                 <a href="http://devtagon.com/" style={{ position: "relative" }} className="stretched-link font-weight-bold text-dark"> Devtagon</a>.Here you can learn more about me and my ideas.
-
+                <div  >
+                    <img alt="baby yoda" style={{ width: 500, borderRadius: 20 }} src="https://www.macleans.ca/wp-content/uploads/2019/11/BABY-YODA-MANDALORIAN-NOV19-810x445.jpg" />
+                </div>
             </div>
 
 
