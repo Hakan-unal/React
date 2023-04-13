1c37c9f36a528723684ab06b5272962e3b8eb9ba
Con Sınov <hakanunal_1996@hotmail.com>
2023-04-13 23:02:08 +0300

    updated

--- src/components/Demos.js
+++ src/components/Demos.js
@@ -37,7 +37,30 @@ export default class Application extends Component {
     return (
       <>
         <Row className="mt-5 mx-3">
-
+          <Col
+            className="mt-2"
+            sm={{ span: 12 }}
+            md={{ span: 6 }}
+            lg={{ span: 4 }}
+            xl={{ span: 3 }}
+          >
+            <Card style={cardPadding}>
+              <Card.Img width={100} height={150} variant="top" src={vercel} />
+              <Card.Body>
+                <Card.Title>Sound Box</Card.Title>
+                <hr />
+                <Card.Text>
+                  React.js application
+                </Card.Text>
+                <Button
+                  variant="outline-primary"
+                  href="https://soundbox-puce.vercel.app/"
+                >
+                  Demo
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
           <Col
             className="mt-2"
             sm={{ span: 12 }}
