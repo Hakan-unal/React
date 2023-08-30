27ff14174864cbe46b654627c4733fd84e73539d
Con Sınov <hakanunal_1996@hotmail.com>
2023-08-30 18:50:11 +0300

    updated

--- src/pages/articles/articles.tsx
+++ src/pages/articles/articles.tsx
@@ -14,7 +14,6 @@ const Articles: React.FC = () => {
         { title: "What is Single Page Application", link: "https://spa-gamma-woad.vercel.app" },
         { title: "Using the File System Module in Node.js", link: "https://file-system-module.vercel.app" },
         { title: "Math, Date, String and Number Classes in JavaScript", link: "https://javascript-class.vercel.app" },
-        { title: "Class vs Functional Component", link: "https://class-vs-functional.vercel.app" },
 
     ]
     return (<Card style={{ textAlign: "center" }} >
