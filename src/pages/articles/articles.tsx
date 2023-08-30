ca6256c15cc20b389b343f335c1e5e6c70b9c346
Con Sınov <hakanunal_1996@hotmail.com>
2023-08-30 18:48:02 +0300

    updated

--- src/pages/articles/articles.tsx
+++ src/pages/articles/articles.tsx
@@ -8,6 +8,7 @@ type Element = {
 
 const Articles: React.FC = () => {
     const articles: Array<Element> = [
+        { title: "Vercel Application Publish", link: "https://vercel-rho-gules.vercel.app/" },
         { title: "Create Read Update Delete (CRUD)", link: "https://hakanunal-crud.vercel.app/" },
         { title: "Monolithic vs Microservices Architecture", link: "https://monolith-mcroservices.vercel.app" },
         { title: "What is Single Page Application", link: "https://spa-gamma-woad.vercel.app" },
