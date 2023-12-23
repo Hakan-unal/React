092b13cefdb67fffef640ad542b167689ffb6460
Con Sınov <hakanunal_1996@hotmail.com>
2023-12-23 17:28:40 +0300

    updated

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,6 +2,7 @@ import { Button, Layout, Space, theme } from "antd"
 import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { BsStackOverflow } from "react-icons/bs";
+import { FaNpm } from "react-icons/fa";
 
 
 
@@ -16,6 +17,8 @@ const Head: React.FC = (...props) => {
             <Button rel="noopener" target='_blank' href={"https://github.com/Hakan-unal"} icon={<AiOutlineGithub size={25} />} />
             <Button rel="noopener" target='_blank' href={"https://stackoverflow.com/users/22632846/hakan-unal"} icon={<BsStackOverflow color='orange' size={25} />} />
             <Button rel="noopener" target='_blank' href={"https://play.google.com/store/apps/dev?id=8883692665902284708"} icon={<FcGoogle size={25} />} />
+            <Button rel="noopener" target='_blank' href={"https://www.npmjs.com/~kardinal"} icon={<FaNpm size={25} color='red' />} />
+
         </Space>
     </Header>)
 }
